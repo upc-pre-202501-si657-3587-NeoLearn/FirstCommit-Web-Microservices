711ba195a791384437f33f7350f8933120bd6159
Lucio Yen <U202213143@upc.edu.pe>
2025-06-09 14:30:19 -0500

    Revert "feat: Add new functional"
    This reverts commit 2c13a31c53280d04c5e529bebf06810bdbb29dcc.

--- firstcommit-microservices/roadmaps-service/src/main/java/com/neolearn/roadmaps_service/domain/repositories/IModuloRepository.java
+++
@@ -1,7 +0,0 @@
-package com.neolearn.roadmaps_service.domain.repositories;
-
-import com.neolearn.roadmaps_service.domain.model.Modulo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IModuloRepository extends JpaRepository<Modulo, Long> {
-}
